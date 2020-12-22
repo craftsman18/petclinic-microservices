d8432319d86b3d0868cd608717af0ae0c926f879
craftsman18 <67903927+craftsman18@users.noreply.github.com>
2020-12-22 22:29:22 +0300

    Update create-ecr-docker-registry-for-dev.sh

--- infrastructure/create-ecr-docker-registry-for-dev.sh
+++ infrastructure/create-ecr-docker-registry-for-dev.sh
@@ -1,5 +1,5 @@
 PATH="$PATH:/usr/local/bin"
-APP_REPO_NAME="craftsman-repo/petclinic-app-dev"
+APP_REPO_NAME="craftsman18-repo/petclinic-app-dev"
 AWS_REGION="us-east-1"
 aws ecr create-repository \
   --repository-name ${APP_REPO_NAME} \
